b6c825816bebac630d8277033dc3953f274de390
shmon <shmonyajik@yandex.ru>
2022-09-20 10:07:41 +0300

    Исправил .gitignore на UTF-8

---
+++ core/quickstart.py
@@ -0,0 +1,49 @@
+
+from loguru import logger
+import os.path
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+SAMPLE_RANGE_NAME = 'Test List!A2:E246'
+
+class GoogleSheet:
+    SPREADSHEET_ID = '16mUOHitGZjIwfAGCxwp0WnihellDiWOjBk3GYT8F6g0'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    service = None
+
+    def __init__(self):
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                logger.info('flow')
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('sheets', 'v4', credentials=creds)
+
+    def updateRangeValues(self, range, values):
+        data = [{
+            'range': range,
+            'values': values
+        }]
+        body = {
+            'valueInputOption': 'USER_ENTERED',
+            'data': data
+        }
+        result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SPREADSHEET_ID, body=body).execute()
+        logger.info('{0} cells updated.'.format(result.get('totalUpdatedCells')))
+
+        
